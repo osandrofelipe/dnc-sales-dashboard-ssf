6a22a376d2c4506ee92404180d3cdd9d1dbc636a
Sandro Felipe <sandrofelipe.services@hotmail.com>
2025-09-03 15:24:17 -0300

    Feat: Create Themes

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { GlobalStyle } from './styles/globalStyle'
-import { lighTheme, darkTheme } from './styles/'
+import { lighTheme } from './styles/'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
