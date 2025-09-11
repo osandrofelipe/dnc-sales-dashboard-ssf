b5c6f45f38e78faeb28ae5dd1cab5ad4ace30552
Sandro Felipe <sandrofelipe.services@hotmail.com>
2025-09-11 16:22:55 -0300

    Feat: Home Page Request

--- src/components/CustomTable.tsx
+++ src/components/CustomTable.tsx
@@ -8,6 +8,21 @@ const TableWrapper = styled.div`
     table {
         width: 100%;
         border-collapse: collapse;
+
+        .ellipsis {
+            display: block;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            overflow: hidden;
+        }
+
+        .ellipsis-sm {
+            width: ${pxToRem(300)};
+        }
+
+        .ellipsis-xs {
+            width: ${pxToRem(150)};
+        }
         th, td {
             height: ${pxToRem(48)};
             padding: 0 ${pxToRem(8)} 0 0;
---
+++ src/types/highlightsData.d.ts
@@ -0,0 +1,6 @@
+export interface HighlightsData {
+    value: number
+    subtitle: string
+}
+
+
